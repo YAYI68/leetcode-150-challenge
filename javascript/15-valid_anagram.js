fe560e4c2a74bf1b69aa6e9098091348131f5390
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2023-09-04 19:01:18 +0100

    commit

---
+++ javascript/15-valid_anagram.js
@@ -0,0 +1,47 @@
+// # Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// # An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// # Example 1:
+
+// # Input: s = "anagram", t = "nagaram"
+// # Output: true
+// # Example 2:
+
+// # Input: s = "rat", t = "car"
+// # Output: false
+
+const validAnagram = (s, t) => {
+  if (s.length !== t.length) return false;
+
+  for (let i = 0; i < t.length; i++) {
+    if (!s.includes(t[i])) return false;
+
+    let loc = s.indexOf(t[i]);
+    s = s.slice(0, loc) + s.slice(loc + 1);
+  }
+  return true;
+};
+
+const validAnagram2 = (s, t) => {
+  if (s.length !== t.length) return false;
+
+  let common = {};
+  for (let i = 0; i < s.length; i++) {
+    s[i] in common ? common[s[i]]++ : (common = 1);
+  }
+
+  for (let j = 0; j < t.length; j++) {
+    if (t[j] in common && common[t[j]] >= 1) {
+      common[t[j]]--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
+
+s = "anagram";
+t = "nagaram";
+
+console.log(validAnagram(s, t));
---
+++ python/15-valid_anagram.py
@@ -0,0 +1,49 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+# Example 1:
+
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+# Example 2:
+
+# Input: s = "rat", t = "car"
+# Output: false
+
+def validAnagram(s: str, t: str):
+    if (len(s) != len(t)):
+        return False
+
+    map1 = {}
+    for ch in s:
+        map1[ch] = map1.get(ch, 0) + 1
+    for char in t:
+        if (char in map1.keys() and map1[char] >= 1):
+            print(map1)
+            map1[char] -= 1
+        else:
+            return False
+    return True
+
+
+def validAnagram2(s: str, t: str):
+    if (len(s) != len(t)):
+        return False
+
+    for ch in t:
+        if ch not in s:
+            return False
+
+        location = s.index(ch)
+        s = s[0:location] + s[location + 1:]
+
+    return True
+
+
+# s = "anagram"
+# t = "nagaram"
+s = "rat"
+t = "car"
+print(validAnagram2(s, t))
